3c59670d0b159ed09cb25ff1cd29617f234faae2
elson lim <elsonlim7@gmail.com>
2019-03-04 23:37:25 +0800

    color theme

---
+++ src/Components/Content/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default () => (
+<div style={styles.container}>
+</div>
+)
+
+const styles = {
+  container: {
+    backgroundColor: '#e6f2ff',
+    flex: 1
+  }
+}
--- src/Components/IfElsonCode/index.js
+++ src/Components/IfElsonCode/index.js
@@ -1,23 +1,47 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+
+import React from 'react'
 import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { blue, indigo, purple } from '@material-ui/core/colors';
 
 const IfElsonCode = () => {
     return (
-        <div>
-             <AppBar position="static">
-                <Toolbar>
-                    <Typography variant="h6" color="inherit" className={styles.grow}>
-                        Elson
-                    </Typography>
-                </Toolbar>
-             </AppBar>
-        </div>
+      <MuiThemeProvider theme={theme}>
+            <AppBar position="sticky">
+              <Toolbar>
+                  <Typography variant="h5" color="inherit" className={styles.grow}>
+                      Blog - Elson
+                  </Typography>
+              </Toolbar>
+            </AppBar>
+      </MuiThemeProvider>
     );
 }
 
 export default IfElsonCode;
+
+const theme = createMuiTheme({
+  palette: {
+    primary: blue,
+  },
+  overrides: {
+    // Name of the component ⚛️ / style sheet
+    MuiButton: {
+      // Name of the rule
+      text: {
+        // Some CSS
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        borderRadius: 3,
+        border: 0,
+        color: 'white',
+        height: 48,
+        padding: '0 30px',
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+      },
+    },
+  },
+  typography: { useNextVariants: true },
+});
 
 const styles = {
   root: {
