04ee30ae1133579516aaf2c91b1907e3bccf6c4c
elson lim <elsonlim7@gmail.com>
2019-04-01 22:28:22 +0800

    fix test

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {render, fireEvent} from 'react-testing-library';
-import App from '../App';
+import App from '../components/App';
 
 describe('App', () => {
   it('renders without crashing', () => {
