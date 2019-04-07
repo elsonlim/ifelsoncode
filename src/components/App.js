85aca97ce869160ed5f18ffb85836277c82897f2
elson lim <elsonlim7@gmail.com>
2019-04-07 23:05:56 +0800

    set up firebase

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,20 @@ import Blog from '../components/Blog';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from '../reducers';
+import { initializeApp } from 'firebase/app';
 
 class App extends Component {
+  componentWillMount() {
+    initializeApp({
+      apiKey: "AIzaSyA2Gk0VffTp7Y4SpjhtEON-MdFV2vyhd14",
+      authDomain: "ifelsoncode.firebaseapp.com",
+      databaseURL: "https://ifelsoncode.firebaseio.com",
+      projectId: "ifelsoncode",
+      storageBucket: "ifelsoncode.appspot.com",
+      messagingSenderId: "1003148850780"
+    });
+  }
+
   render() {
     return (
       <Provider store={createStore(reducers)}>
